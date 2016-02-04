185091b391a9035e82906f58e630949bbc53d838
hatiolab <shortstop@hatiolab.com>
2016-02-04 21:31:24 +0900

    Ajax Request Hooking

--- vendor/bundles/hatiocore/app/assets/javascripts/app-std/app.js
+++ vendor/bundles/hatiocore/app/assets/javascripts/app-std/app.js
@@ -9,6 +9,27 @@ Ext.Loader.setConfig({
 
 Ext.require(['App.Framework']);
 
+/*Ext.define('App.util.AppBaseUrl',{
+    singleton : true,
+    alternateClassName : 'AppBaseUrl',
+    requires:['Ext.Ajax'],
+ 
+    config: {
+        baseUrl:'http://localhost:9002/rest/'
+    },
+ 
+    constructor : function(config) {
+            this.initConfig(config);
+            Ext.Ajax.on('beforerequest', this.onBeforeAjaxRequest, this);
+    },
+ 
+    onBeforeAjaxRequest : function(connection, options) {
+              options.url = this.getBaseUrl() + options.url;
+    }
+});
+
+Ext.require(['App.Framework', 'App.util.AppBaseUrl']);*/
+
 Ext.onReady(function() {
 	Ext.application({
 		appFolder: 'assets/app-std',
