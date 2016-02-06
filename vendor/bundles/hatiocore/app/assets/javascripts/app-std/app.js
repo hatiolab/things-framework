16af0606810cd40e3d6b633137638901b1bf3b7d
hatiolab <shortstop@hatiolab.com>
2016-02-06 21:56:23 +0900

    Service Hook 제거

--- vendor/bundles/hatiocore/app/assets/javascripts/app-std/app.js
+++ vendor/bundles/hatiocore/app/assets/javascripts/app-std/app.js
@@ -7,182 +7,182 @@ Ext.Loader.setConfig({
 	bundleRoot : 'assets/bundle/'
 });
 
-//Ext.require(['App.Framework']);
+Ext.require(['App.Framework']);
 
-Ext.define('App.util.AppBaseUrl',{
-    singleton : true,
-    alternateClassName : 'AppBaseUrl',
-    requires:['Ext.Ajax'],
- 
-    config: {
-        baseUrl:'http://localhost:9002/rest/'
-    },
- 
-    constructor : function(config) {
-		this.initConfig(config);
-		Ext.Ajax.on('beforerequest', this.onBeforeAjaxRequest, this);
-    },
- 
-    onBeforeAjaxRequest : function(connection, options) {
-		if(options && options.method == 'PUT' && options.action == 'update') {
-			this.hookRestUpdate(options);
+// Ext.define('App.util.AppBaseUrl',{
+//     singleton : true,
+//     alternateClassName : 'AppBaseUrl',
+//     requires:['Ext.Ajax'],
+//  
+//     config: {
+//         baseUrl:'http://localhost:9002/rest/'
+//     },
+//  
+//     constructor : function(config) {
+// 		this.initConfig(config);
+// 		Ext.Ajax.on('beforerequest', this.onBeforeAjaxRequest, this);
+//     },
+//  
+//     onBeforeAjaxRequest : function(connection, options) {
+// 		if(options && options.method == 'PUT' && options.action == 'update') {
+// 			this.hookRestUpdate(options);
 
-		} else if(options && options.method == 'GET' && options.action == 'read' && options.params && !options.params.id) {
-			this.hookRestSearch(options.params); 
-		} 
+// 		} else if(options && options.method == 'GET' && options.action == 'read' && options.params && !options.params.id) {
+// 			this.hookRestSearch(options.params); 
+// 		} 
 
-		options.url = this.getBaseUrl() + options.url;
-    },
+// 		options.url = this.getBaseUrl() + options.url;
+//     },
 
-	/**
-	 * Update 요청시 hooking
-	 */
-	hookRestUpdate : function(options) {
-		if(options.scope && options.scope.writer && options.scope.writer.root) {
-			var rootName = options.scope.writer.root;
-			var jsonData = options.jsonData[rootName];
-			if(jsonData) {
-				Ext.Array.each(['creator', 'updater', 'creator_id', 'created_at', 'updater_id', 'updated_at'], function(key) {
-					delete jsonData[key];
-				});
-				options.jsonData = jsonData;
-			}
-		}
-	},
+// 	/**
+// 	 * Update 요청시 hooking
+// 	 */
+// 	hookRestUpdate : function(options) {
+// 		if(options.scope && options.scope.writer && options.scope.writer.root) {
+// 			var rootName = options.scope.writer.root;
+// 			var jsonData = options.jsonData[rootName];
+// 			if(jsonData) {
+// 				Ext.Array.each(['creator', 'updater', 'creator_id', 'created_at', 'updater_id', 'updated_at'], function(key) {
+// 					delete jsonData[key];
+// 				});
+// 				options.jsonData = jsonData;
+// 			}
+// 		}
+// 	},
 
-	/**
-	 * Search 요청시 hooking
-	 */
-	hookRestSearch : function(params) {
-		if(params._s && params._s != '') {
-			this.convertSelectFields(params);
-		}
+// 	/**
+// 	 * Search 요청시 hooking
+// 	 */
+// 	hookRestSearch : function(params) {
+// 		if(params._s && params._s != '') {
+// 			this.convertSelectFields(params);
+// 		}
 
-		if(params.sort && params.sort != '') {
-			this.convertSortConditions(params);
-		} 
+// 		if(params.sort && params.sort != '') {
+// 			this.convertSortConditions(params);
+// 		} 
 
-		this.convertSearchConditions(params);
-	},
+// 		this.convertSearchConditions(params);
+// 	},
 
-	/**
-	 * select fields camelcase로 변환 
-	 */
-	convertSelectFields : function(params) {
-		var oriSelects = params._s.substr(1, params._s.length - 2);
-		delete params['_s'];
-		var selectArr = oriSelects.split(',');
+// 	/**
+// 	 * select fields camelcase로 변환 
+// 	 */
+// 	convertSelectFields : function(params) {
+// 		var oriSelects = params._s.substr(1, params._s.length - 2);
+// 		delete params['_s'];
+// 		var selectArr = oriSelects.split(',');
 
-		params.select = '';
-		for(var i = 0 ; i < selectArr.length ; i++) {
-			if(i > 0) {
-				params.select += ',';
-			}
-			params.select += HF.camelize(selectArr[i]);
-		}
-	},
+// 		params.select = '';
+// 		for(var i = 0 ; i < selectArr.length ; i++) {
+// 			if(i > 0) {
+// 				params.select += ',';
+// 			}
+// 			params.select += HF.camelize(selectArr[i]);
+// 		}
+// 	},
 
-	/**
-	 * sort condition 변환 
-	 * [{"property":"updated_at","direction":"ASC"}] ==> [{"name":"updatedAt","dir":"ASC"}] 형식으로 변환 
-	 */
-	convertSortConditions : function(params) {
-		if(params.sort && params.sort != '') {
-			var sortArr = Ext.JSON.decode(params.sort);
+// 	/**
+// 	 * sort condition 변환 
+// 	 * [{"property":"updated_at","direction":"ASC"}] ==> [{"name":"updatedAt","dir":"ASC"}] 형식으로 변환 
+// 	 */
+// 	convertSortConditions : function(params) {
+// 		if(params.sort && params.sort != '') {
+// 			var sortArr = Ext.JSON.decode(params.sort);
 
-			for(var i = 0 ; i < sortArr.length ; i++) {
-				var sortObj = sortArr[i];
-				var fieldName = HF.camelize(sortObj.property);
-				var direction = sortObj.direction;
-				sortObj['name'] = fieldName;
-				sortObj['dir'] = direction;
-				delete sortObj['property'];
-				delete sortObj['direction'];
-			}
+// 			for(var i = 0 ; i < sortArr.length ; i++) {
+// 				var sortObj = sortArr[i];
+// 				var fieldName = HF.camelize(sortObj.property);
+// 				var direction = sortObj.direction;
+// 				sortObj['name'] = fieldName;
+// 				sortObj['dir'] = direction;
+// 				delete sortObj['property'];
+// 				delete sortObj['direction'];
+// 			}
 
-			params.sort = Ext.JSON.encode(sortArr);
-		}
-	},
+// 			params.sort = Ext.JSON.encode(sortArr);
+// 		}
+// 	},
 
-	/**
-	 * search conditions 변환 
-	 */
-	convertSearchConditions : function(params) {
-		var searchKeys = this.extractSearchConditions(params);
+// 	/**
+// 	 * search conditions 변환 
+// 	 */
+// 	convertSearchConditions : function(params) {
+// 		var searchKeys = this.extractSearchConditions(params);
 
-		if(searchKeys && searchKeys.length > 0) {
-			var query = [];
+// 		if(searchKeys && searchKeys.length > 0) {
+// 			var query = [];
 
-			for(var i = 0 ; i < searchKeys.length ; i++) {
-				var key = searchKeys[i];
+// 			for(var i = 0 ; i < searchKeys.length ; i++) {
+// 				var key = searchKeys[i];
 
-				if(!this.skipConvertSearchCondition(params, key)) {
-					var prop = key.substr(3, key.length - 4);
-					query.push(this.convertSearchCondition(params, prop, params[key]));
-				}
+// 				if(!this.skipConvertSearchCondition(params, key)) {
+// 					var prop = key.substr(3, key.length - 4);
+// 					query.push(this.convertSearchCondition(params, prop, params[key]));
+// 				}
 
-				delete params[key];
-			}
+// 				delete params[key];
+// 			}
 
-			if(query.length > 0) {
-				params.query = '[' + query.join(',') + ']';
-			}
-		}
-	},
+// 			if(query.length > 0) {
+// 				params.query = '[' + query.join(',') + ']';
+// 			}
+// 		}
+// 	},
 
-	/**
-	 * 검색 조건 변환 스킵 여부  
-	 */
-	skipConvertSearchCondition : function(params, propName) {
-		var skip = false;
+// 	/**
+// 	 * 검색 조건 변환 스킵 여부  
+// 	 */
+// 	skipConvertSearchCondition : function(params, propName) {
+// 		var skip = false;
 
-		if(!params[propName]) {
-			skip = true;
-		}
+// 		if(!params[propName]) {
+// 			skip = true;
+// 		}
 
-		if(skip == true) {
-			Ext.Array.each(['is_null]', 'is_not_null]', 'is_true]', 'is_false]', 'is_present]', 'is_blank]'], function(key) {
-				if(propName.indexOf(key, propName.length - key.length) !== -1) {
-					skip = false;
-				} 
-			});
-		}
+// 		if(skip == true) {
+// 			Ext.Array.each(['is_null]', 'is_not_null]', 'is_true]', 'is_false]', 'is_present]', 'is_blank]'], function(key) {
+// 				if(propName.indexOf(key, propName.length - key.length) !== -1) {
+// 					skip = false;
+// 				} 
+// 			});
+// 		}
 
-		return skip;
-	},
+// 		return skip;
+// 	},
 
-	/**
-	 * search condition 변환 
-	 * {_q[entity_type-eq] : 'abc'} 형식의 파라미터를 {'name' : 'entityType', 'operator' : 'eq', 'value' : 'abc'} 형식의 문자열로 변환 
-	 */
-	convertSearchCondition : function(params, prop, value) {
-		var propArr = prop.split('-');
-		var operator = propArr[1] || 'eq';
-		var name = HF.camelize(propArr[0]);
-		value = (value == undefined) ? null : value;
-		return '{"name":"' + name + '", "operator":"' + operator + '", "value":"' + value + '"}';
-	},
+// 	/**
+// 	 * search condition 변환 
+// 	 * {_q[entity_type-eq] : 'abc'} 형식의 파라미터를 {'name' : 'entityType', 'operator' : 'eq', 'value' : 'abc'} 형식의 문자열로 변환 
+// 	 */
+// 	convertSearchCondition : function(params, prop, value) {
+// 		var propArr = prop.split('-');
+// 		var operator = propArr[1] || 'eq';
+// 		var name = HF.camelize(propArr[0]);
+// 		value = (value == undefined) ? null : value;
+// 		return '{"name":"' + name + '", "operator":"' + operator + '", "value":"' + value + '"}';
+// 	},
 
-	/**
-	 * Search Condition Key를 추출 
-	 */
-	extractSearchConditions : function(params) {
-		var searchKeys = [];
+// 	/**
+// 	 * Search Condition Key를 추출 
+// 	 */
+// 	extractSearchConditions : function(params) {
+// 		var searchKeys = [];
 
-		for(var key in params) {
-			if(!params.hasOwnProperty(key)) continue;
+// 		for(var key in params) {
+// 			if(!params.hasOwnProperty(key)) continue;
 
-			// '_q['로 시작하면 ...
-			if(key.lastIndexOf('_q[', 0) === 0) {
-				searchKeys.push(key);
-			}
-		}
+// 			// '_q['로 시작하면 ...
+// 			if(key.lastIndexOf('_q[', 0) === 0) {
+// 				searchKeys.push(key);
+// 			}
+// 		}
 
-		return searchKeys;
-	}
-});
+// 		return searchKeys;
+// 	}
+// });
 
-Ext.require(['App.Framework', 'App.util.AppBaseUrl']);
+// Ext.require(['App.Framework', 'App.util.AppBaseUrl']);
 
 Ext.onReady(function() {
 	Ext.application({
