557cf3f7113019aeb6adc31276e0a16cef52ba2d
hatiolab <shortstop@hatiolab.com>
2016-02-18 17:12:53 +0900

    DiyService Logic Editor 언어에 따라 자동 설정되도록 수정

--- vendor/bundles/base/app/assets/javascripts/bundle/base/view/diy_service/DiyServiceLogic.js
+++ vendor/bundles/base/app/assets/javascripts/bundle/base/view/diy_service/DiyServiceLogic.js
@@ -48,15 +48,27 @@ Ext.define('Base.view.diy_service.DiyServiceLogic', {
 	
 	loadRecord : function(record) {
 		this.callParent(arguments);
-		
 		var comp = this.down('component#editor');
+
 		if(comp.editor) {
 			// 화면이 액티브한 상태가 아니면, 잘 리프레쉬되지 않는 문제가 있다. 강제할 수 있는 방법이 필요.
-			comp.editor.setValue(record.get('service_logic'))
+			comp.editor.setValue(record.get('service_logic'));
+			// Editor에 언어에 맞는 Editor를 설정한다.
+			this.setEditorLanguage(record, comp.editor);
 		} else {
 			comp.update({
 				logic : record.get('service_logic')
 			});
+		}
+	},
+
+	setEditorLanguage : function(record, editor) {
+		if('ruby' == record.get('lang_type')) {
+			editor.getSession().setMode("ace/mode/ruby");
+		} else if('groovy' == record.get('lang_type')) {
+			editor.getSession().setMode("ace/mode/groovy");
+		} else {
+			editor.getSession().setMode("ace/mode/javascript");
 		}
 	}
 });
